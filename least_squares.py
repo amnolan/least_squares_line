d84f0b14605312d8e20c0140b00e05d21f31b93b
A.M. Nolan <11098245+amnolan@users.noreply.github.com>
2023-11-12 10:27:57 -0600

    Update least_squares.py working

--- least_squares.py
+++ least_squares.py
@@ -13,6 +13,15 @@ def two_d_deep_copy(src_array):
         dest_array.append(sub_ra)
     return dest_array
 
+def total_up_mean_matrix(ra):
+    totals_list = []
+    for i in range(0,len(ra)):
+        total = 0
+        for j in range(0,len(ra[i])):
+            total += ra[i][j]
+        totals_list.append(total)
+    return totals_list
+
 def transpose_matrix(matrix):
 
     rows = len(matrix)
@@ -44,12 +53,7 @@ def matrix_multiply(matrix1, matrix2):
                 result_matrix[i][j] += matrix1[i][k] * matrix2[k][j]
 
     return result_matrix
-
-def multiply_matrix_by_scalar(matrix, scalar):
-    # Iterate through the matrix and multiply each element by the scalar
-    result_matrix = [[element * scalar for element in row] for row in matrix]
-    return result_matrix
-
+    
 def fill_ones(x_ra):
     ret_ra = []
     ones = []
@@ -75,6 +79,30 @@ def build_w(scale1, scale2, length):
         two_d_w_outer.append(row)
     return two_d_w_outer
 
+def find_inverse(matrix):
+    try:
+        # Check if the matrix is 2x2
+        if len(matrix) != 2 or len(matrix[0]) != 2 or len(matrix[1]) != 2:
+            raise ValueError("Input matrix must be a 2x2 matrix")
+
+        # Get matrix elements
+        a, b, c, d = matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
+
+        # Calculate the determinant
+        determinant = a * d - b * c
+
+        # Check if the matrix is invertible
+        if determinant == 0:
+            raise ValueError("Matrix is not invertible")
+
+        # Calculate the inverse
+        inverse_matrix = [[d/determinant, -b/determinant], [-c/determinant, a/determinant]]
+
+        return inverse_matrix
+
+    except Exception as e:
+        return "Error: "+str(e)
+
 print("Finding least squares line")
 print("(WX)^T*WXß)=(WX)^T*WY")
 print("Input all X values of pairs comma separated")
@@ -95,10 +123,31 @@ y_trans = transpose_matrix([bottom_row])
 print(y_trans)
 print("Enter scale if one half then 1,2")
 scale = convert_to_float((input()).split(","))
-print("Finding WX")
+print("Finding W")
 w_matrix = build_w(scale[0],scale[1],len(y_trans))
 print(w_matrix)
-
-#save the original state
-# original_ra = two_d_deep_copy(parent_ra)
-# matrix_multiply()
+print("Any key")
+input()
+original_ra = two_d_deep_copy(parent_ra)
+# find WX
+wx_mtrx = matrix_multiply(w_matrix,x_trans)
+wy_mtrx = matrix_multiply(w_matrix,y_trans)
+print("Finding WX")
+print(wx_mtrx)
+print("Any key")
+input()
+print("Finding WY")
+print(wy_mtrx)
+print("Any key")
+input()
+wxt_mtrx = transpose_matrix(wx_mtrx)
+wxt_wy_mtrx = matrix_multiply(wxt_mtrx,wy_mtrx)
+wxt_wx_mtrx = matrix_multiply(wxt_mtrx,wx_mtrx)
+print("finding WX^T * WY")
+print(wxt_wy_mtrx)
+print("finding WX^T * WX")
+print(wxt_wx_mtrx)
+wxt_wx_invers_mtrx = find_inverse(wxt_wx_mtrx)
+print("finding (wx^t*wx) inverse (final result)")
+res = matrix_multiply(wxt_wx_invers_mtrx,wxt_wy_mtrx)
+print(res)
